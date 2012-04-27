a7f1766f701e1dcedc2140c670961e308946d52f
Justin <yust09@gmail.com>
2012-04-27 15:40:03 +0200

    Config.assets.manifest add ine config/envi/dev.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ SampleApp::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.manifest = Rails.root.join("public/assets")
+
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
